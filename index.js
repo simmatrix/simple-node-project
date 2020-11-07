779be4cbd5e6406eeb9f5d1ab3f6d1df1ea2b28c
Sim <sim@wego.com>
2020-11-07 15:56:17 +0800

    chore: Rename views template folder

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.set('views', path.join(__dirname, '/assets'))
+app.set('views', path.join(__dirname, '/views'))
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
